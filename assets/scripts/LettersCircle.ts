153b413c958d8abb153aaf7359dd2014c014f8b8
Mariya <m.lebadina@freeplay.io>
2024-10-09 17:54:01 +0300

    init project

---
+++ assets/scripts/LettersCircle.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('LettersCircle')
+export class LettersCircle extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
